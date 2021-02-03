7577792dfb4101d631e886d280d2710cf6c67b05
Beau <beau.williams@icloud.com>
2021-02-04 07:39:21 +1100

    Removing crud

--- lua/utils.lua
+++ lua/utils.lua
@@ -3,46 +3,6 @@ local api = vim.api
 
 local utils = {}
 
-local function map(type, input, output)
-    api.nvim_set_keymap(type, input, output, {})
-end
-
-local function noremap(type, input, output)
-    api.nvim_set_keymap(type, input, output, { noremap = true })
-end
-
-function nnoremap(input, output)
-    noremap('n', input, output)
-end
-
-function inoremap(input, output)
-    noremap('i', input, output)
-end
-
-function vnoremap(input, output)
-    noremap('v', input, output)
-end
-
-function tnoremap(input, output)
-    noremap('t', input, output)
-end
-
-function nmap(input, output)
-	map('n', input, output)
-end
-
-function imap(input, output)
-	map('i', input, output)
-end
-
-function vmap(input, output)
-	map('v', input, output)
-end
-
-function tmap(input, output)
-	map('t', input, output)
-end
-
 -- returns nil if not exists
 function utils.is_dir(filepath)
     local ok, _ = os.rename(filepath, filepath)
