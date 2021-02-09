a513a45991e53612f3ea9431eaa71a130a6f92cb
beauwilliams <beau.williams@icloud.com>
2021-02-09 13:51:08 +1100

    Moved coc to its own file

---
+++ lua/sections/_coc.lua
@@ -0,0 +1,10 @@
+local utils = require '_utils'
+local M = {}
+-- neoclide/coc.nvim
+function M.cocStatus()
+  local cocstatus = ''
+  if vim.fn.exists('*coc#status') == 0 then return '' end
+    cocstatus = utils.Call('coc#status', {})
+  return cocstatus
+end
+return M
--- lua/statusline.lua
+++ lua/statusline.lua
@@ -166,13 +166,6 @@ function M.isBufferModified()
 end
 
 
--- neoclide/coc.nvim
-function M.cocStatus()
-  local cocstatus = ''
-  if vim.fn.exists('*coc#status') == 0 then return '' end
-    cocstatus = utils.Call('coc#status', {})
-  return cocstatus
-end
 
 
 
